2b14802818cd4012b628d30529bde7509e3d02a0
Sofia Hudson <110602213+metis89@users.noreply.github.com>
2023-03-02 20:36:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,43 +1,76 @@
-//Estas funciónes recogen el nuémero
+//Estas funciónes recogen el número
 
 let receiveNumber1;
 let receiveNumber2;
 
 const askNumber1 = () => {
-    let receiveNumber1 = prompt('Introduce el primer número');
+    let receiveNumber1 = Number(prompt("Introduce número"));
     return receiveNumber1;
 };
 
 const askNumber2 = () => {
-    let receiveNumber2 = prompt('Introduce el segundo número')
+    let receiveNumber2 = Number(prompt("Introduce número" ));
     return receiveNumber2;
 };
 
 numero1 = askNumber1();
 numero2 = askNumber2();
 
-console.log(numero1);
-console.log(numero2);
-
 
 //Estas funciónes realizan el cálculo
-const sum = (valueA, valueB) => valueA + valueB 
-    const sumTwoNumbers = sum(numero1, numero2);
-    return alert(sumTwoNumbers);
+
+//Raiz cuadrada:
+const raiz = (Valor1, Valor2) => {
+    if(Valor1 === "0" || Valor2 === "0"){
+       resultadoRaiz = Math.sqrt(Valor1 + Valor2);
+       return ("El resultado de la raiz es " + resultadoRaiz);
+    }  
+}
+
+r = raiz(numero1, numero2);
+alert(r);
 
 
-const substract = (valueA, valueB) => valueA - valueB 
-    const subTwoNumbers = substract(numero1, numero2);
-    return alert(subTwoNumbers);
+//Suma:
+const plus = (Valor1, Valor2) => {
+    resultadoSuma = Valor1 + Valor2;
+    return ('El resultado de la suma es ' + resultadoSuma); 
+}    
+suma = plus(numero1, numero2)
 
-const division = (valueA, valueB) => valueA / valueB 
-    const divTwoNumbers = division(numero1, numero2)
-    return alert(divTwoNumbers);
+alert(suma);
+
+//Resta:
+const minus = (Valor1, Valor2) => {
+    resultadoResta = Valor1 - Valor2;
+    return ('El resultado de la resta es ' + resultadoResta); 
+}    
+resta = minus(numero1, numero2)
+
+alert(resta);
+
+//Division:
+
+const divisionar = (Valor1, Valor2) => {
+    resultadoDivision = Valor1 / Valor2;
+    return ('El resultado de la división es ' + resultadoDivision); 
+}    
+divisionacion = divisionar(numero1, numero2);
+
+alert(divisionacion);
+
+//Multiplicación:
+
+const multiply = (Valor1, Valor2) => {
+    resultadoMulti = Valor1 * Valor2;
+    return ('El resultado de la multiplicación es ' + resultadoMulti); 
+}    
+multiplicacion = multiply(numero1, numero2)
+
+alert(multiplicacion);
 
 
-const multiplication = (valueA, valueB) => valueA * valueB 
-    const multiTwoNumbers = multiplication(numero1, numero2);
-    return alert(divTwoNumbers);
+    
 
 
 
