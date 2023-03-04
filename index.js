eb80bb5fcdeb460e4a74704bef1a9556e6050bc9
Sofia Hudson <110602213+metis89@users.noreply.github.com>
2023-03-04 15:13:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -17,16 +17,25 @@ numero1 = askNumber1();
 numero2 = askNumber2();
 
 
+//Errores y avisos
+const letra = (Valor1, Valor2) => {
+    if(isNaN(Valor1) || isNaN(Valor2)){
+        return "Lo que ha introducido no es un número!";
+    }  
+}
+noEsNumero = letra(numero1, numero2);
+alert(noEsNumero);
+
+
 //Estas funciónes realizan el cálculo
 
 //Raiz cuadrada:
 const raiz = (Valor1, Valor2) => {
-    if(Valor1 === "0" || Valor2 === "0"){
+    if(Valor1 === 0 || Valor2 === 0){
        resultadoRaiz = Math.sqrt(Valor1 + Valor2);
        return ("El resultado de la raiz es " + resultadoRaiz);
     }  
 }
-
 r = raiz(numero1, numero2);
 alert(r);
 
@@ -37,8 +46,8 @@ const plus = (Valor1, Valor2) => {
     return ('El resultado de la suma es ' + resultadoSuma); 
 }    
 suma = plus(numero1, numero2)
-
 alert(suma);
+
 
 //Resta:
 const minus = (Valor1, Valor2) => {
@@ -46,8 +55,8 @@ const minus = (Valor1, Valor2) => {
     return ('El resultado de la resta es ' + resultadoResta); 
 }    
 resta = minus(numero1, numero2)
-
 alert(resta);
+
 
 //Division:
 
@@ -56,8 +65,8 @@ const divisionar = (Valor1, Valor2) => {
     return ('El resultado de la división es ' + resultadoDivision); 
 }    
 divisionacion = divisionar(numero1, numero2);
-
 alert(divisionacion);
+
 
 //Multiplicación:
 
@@ -65,14 +74,13 @@ const multiply = (Valor1, Valor2) => {
     resultadoMulti = Valor1 * Valor2;
     return ('El resultado de la multiplicación es ' + resultadoMulti); 
 }    
-multiplicacion = multiply(numero1, numero2)
-
+multiplicacion = multiply(numero1, numero2);
 alert(multiplicacion);
 
 
-    
+//Resultados reunidos en un solo mensaje:
 
-
-
+let array = [suma, resta, divisionacion, multiplicacion];
+alert(array.join('\n'));
 
 
