dda76f6e1c66e6ac8a2166ed1b3c6a1a0afa6ea4
Sofia Hudson <110602213+metis89@users.noreply.github.com>
2023-03-05 21:01:59 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,86 +1,112 @@
+
+//Primero definimos las funciones a usar en el programa
+
 //Estas funciónes recogen el número
-
-let receiveNumber1;
-let receiveNumber2;
-
-const askNumber1 = () => {
-    let receiveNumber1 = Number(prompt("Introduce número"));
-    return receiveNumber1;
-};
-
-const askNumber2 = () => {
-    let receiveNumber2 = Number(prompt("Introduce número" ));
-    return receiveNumber2;
-};
-
-numero1 = askNumber1();
-numero2 = askNumber2();
-
-
-//Errores y avisos
-const letra = (Valor1, Valor2) => {
-    if(isNaN(Valor1) || isNaN(Valor2)){
-        return "Lo que ha introducido no es un número!";
-    }  
+const askElements = () => {
+    let numeroElementos = Number(prompt("¿Con cuantos números quieres operar?"));
+    while(isNaN(numeroElementos)){
+        numeroElementos = prompt("Por favor introduce en número la cantidad de elementos con la que quieres operar");
+    }
+    return numeroElementos;
 }
-noEsNumero = letra(numero1, numero2);
-alert(noEsNumero);
+const operandos = new Array();
+
 
 
 //Estas funciónes realizan el cálculo
 
-//Raiz cuadrada:
-const raiz = (Valor1, Valor2) => {
-    if(Valor1 === 0 || Valor2 === 0){
-       resultadoRaiz = Math.sqrt(Valor1 + Valor2);
-       return ("El resultado de la raiz es " + resultadoRaiz);
-    }  
-}
-r = raiz(numero1, numero2);
-alert(r);
-
-
 //Suma:
-const plus = (Valor1, Valor2) => {
-    resultadoSuma = Valor1 + Valor2;
-    return ('El resultado de la suma es ' + resultadoSuma); 
-}    
-suma = plus(numero1, numero2)
-alert(suma);
+
+let sumacion = 0;
+const suma = () => {
+    for(let i=0; i<cifrasOperar; i++){
+        if (i===0){
+            sumacion = operandos[0];
+            }else{
+            sumacion = sumacion + operandos [i];
+    }
+}
+    return 'El resultado de la suma es ' + sumacion;
+}
+
+
 
 
 //Resta:
-const minus = (Valor1, Valor2) => {
-    resultadoResta = Valor1 - Valor2;
-    return ('El resultado de la resta es ' + resultadoResta); 
-}    
-resta = minus(numero1, numero2)
-alert(resta);
+
+let restacion = 0;
+const resta = () => {
+    for(let i=0; i<cifrasOperar; i++){
+        if (i===0){
+        restacion = operandos[0];
+        }else{
+        restacion = restacion - operandos [i];
+        }
+    }
+    return 'El resultado de la resta es ' + restacion;
+}
 
 
-//Division:
 
-const divisionar = (Valor1, Valor2) => {
-    resultadoDivision = Valor1 / Valor2;
-    return ('El resultado de la división es ' + resultadoDivision); 
-}    
-divisionacion = divisionar(numero1, numero2);
-alert(divisionacion);
+//División:
+
+let division = 0;
+const divide = () => {
+    for(let i=0; i<cifrasOperar; i++){
+        if (i===0){
+            division = operandos[0];
+            }else{
+            division = division / operandos [i];
+        }
+    }
+    return 'El resultado de la división es ' + division;
+}
+
+
 
 
 //Multiplicación:
 
-const multiply = (Valor1, Valor2) => {
-    resultadoMulti = Valor1 * Valor2;
-    return ('El resultado de la multiplicación es ' + resultadoMulti); 
-}    
-multiplicacion = multiply(numero1, numero2);
-alert(multiplicacion);
+let multiplicacion = 0;
+const multiplica = () => {
+    for(let i=0; i<cifrasOperar; i++){
+        if (i===0){
+            multiplicacion = operandos[0];
+            }else{
+            multiplicacion = multiplicacion * operandos [i];
+            }
+    }
+    return 'El resultado de la multiplicación es ' + multiplicacion;
+    
+}
 
+
+
+
+//Ahora invocaremos a las funciones ya definidas anteriormente para ejecutar el programa al completo
+
+let nuevoCalculo = 'Si';
+
+while (nuevoCalculo == 'Si'){
+
+cifrasOperar = askElements();
+
+for(let i=0; i<cifrasOperar; i++){
+    operandos[i] = Number(prompt("Introduce el operador de la posición " + i));
+}
+let totalsuma = suma();
+let totalresta = resta();
+let totaldivision = divide();
+let totalmultiplicacion = multiplica();
 
 //Resultados reunidos en un solo mensaje:
 
-let array = [suma, resta, divisionacion, multiplicacion];
+let array = [totalsuma, totalresta, totaldivision, totalmultiplicacion];
 alert(array.join('\n'));
+
+nuevoCalculo = prompt('¿Quieres volver a hacer "calculaciones", jojojojo? Contesta Si/No')
+
+
+} 
 
 
